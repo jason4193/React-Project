f11003b9cac18130967464c13e9f75109370a07d
{jason4193} <{jason4193@gmail.com}>
2024-05-09 20:42:28 +1000

    Update the favicon on the portfolio site

--- portfolio-v1/src/App.jsx
+++ portfolio-v1/src/App.jsx
@@ -1,12 +1,12 @@
 
-import './App.module.css'
+import styles from './App.module.css'
 
 function App() {
 
   return (
-    <>
-      Hello World
-    </>
+    <div className={styles.App}>
+      <p>Hello World</p>
+    </div>
   )
 }
 
